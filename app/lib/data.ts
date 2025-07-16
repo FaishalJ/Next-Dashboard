6b8285659b7ca98ccaf6bf08e3b42e24ea005990
faishaljaffar <faishaljaffar@gmail.com>
2025-07-16 17:52:29 +0000

    Cleared for upload

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
-import postgres from 'postgres';
+/*
+// import postgres from 'postgres';
 import {
   CustomerField,
   CustomersTableType,
@@ -215,4 +216,9 @@ export async function fetchFilteredCustomers(query: string) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
   }
+}
+*/
+
+export async function fetchInvoicesPages(query: string) {
+  return query;
 }
